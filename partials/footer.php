0bab40c5995077816e5e3c2c1c64c08c78c6338a
Alen Pepa <xalenpepa@gmail.com>
2026-09-15 15:20:59 -0700

    Add files via upload

---
+++ partials/footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * CodeVault shared footer.
+ * Copyright (c) 2026 Alen Pepa. All rights reserved.
+ */
+?>
+</main><?php if(empty($adminShell)): ?><footer><a class="brand" href="index.php"><span>&lt;/&gt;</span> CodeVault</a><p><?= e(t('footer.tagline')) ?></p><p>© <?= date('Y') ?> Alen Pepa. <?= e(t('footer.rights')) ?></p></footer><?php endif; ?></body></html>
